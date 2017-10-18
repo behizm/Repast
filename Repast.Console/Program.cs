3748757aa248c11107f64dea3797896ad7656927
behizm <behizm@gmail.com>
2017-10-18 09:04:08 +0330

    Add project files.

---
+++ Repast.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Repast.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
